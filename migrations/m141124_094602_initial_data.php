99f29eb02bce7818fa5568bf646abe72db7d3de5
Christopher Stebe <c.stebe@herzogkommunikation.de>
2015-01-22 12:06:53 +0100

    update migration class name date

--- migrations/m141124_094602_initial_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use yii\db\Schema;
-use yii\db\Migration;
-
-class m141124_094602_initial_data extends \dmstr\db\mysql\FileMigration
-{
-}
---
+++ migrations/m150124_094602_initial_data.php
@@ -0,0 +1,8 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150124_094602_initial_data extends \dmstr\db\mysql\FileMigration
+{
+}
