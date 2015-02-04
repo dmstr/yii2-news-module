a9da0c9abff9fa79ef63e61bdcfa171087184924
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-02-04 16:11:35 +0100

    fix namespaces in crud.sh

--- crud.sh
+++ crud.sh
@@ -2,7 +2,7 @@
 ./yii giiant-batch --interactive=0 --overwrite=1 \
 --tables=dmstr_news,dmstr_text_block,dmstr_image_gallery,dmstr_image,dmstr_video_gallery,dmstr_video \
 --tablePrefix=dmstr_ \
---modelNamespace=dmstr\\news\\models \
---crudControllerNamespace=dmstr\\news\\controllers \
+--modelNamespace=dmstr\\modules\\news\\models \
+--crudControllerNamespace=dmstr\\modules\\news\\controllers \
 --crudProviders=hrzg\\moxiecode\\moxiemanager\\providers\\Provider \
---modelBaseClass=dmstr\\news\\models\\ActiveRecord
+--modelBaseClass=dmstr\\modules\\news\\models\\ActiveRecord
