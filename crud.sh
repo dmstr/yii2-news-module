ae32762ef9efe5c4d21471cc41dcf3f0dfa2e1de
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-02-04 16:17:53 +0100

    update batch-command

--- crud.sh
+++ crud.sh
@@ -2,7 +2,12 @@
 ./yii giiant-batch --interactive=0 --overwrite=1 \
 --tables=dmstr_news,dmstr_text_block,dmstr_image_gallery,dmstr_image,dmstr_video_gallery,dmstr_video \
 --tablePrefix=dmstr_ \
+--enableI18N=1 \
 --modelNamespace=dmstr\\modules\\news\\models \
 --crudControllerNamespace=dmstr\\modules\\news\\controllers \
+--crudSearchModelNamespace=dmstr\\modules\\news\\models\\search \
+--crudPathPrefix= \
+--messageCategory=app \
+--crudViewPath=@app/vendor/dmstr/yii2-news-module/views \
 --crudProviders=hrzg\\moxiecode\\moxiemanager\\providers\\Provider \
 --modelBaseClass=dmstr\\modules\\news\\models\\ActiveRecord
